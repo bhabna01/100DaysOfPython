33da7faa365ce383316d392827eb3e4dcb692b10
bhabna01 <abierhoque01@gmail.com>
2023-04-26 19:40:02 +0600

    encoding decoding solved

--- day40.py
+++ day40.py
@@ -3,24 +3,40 @@ import string
 import random
 
 letters = string.ascii_lowercase
-word = input("Enter a word:")
+
 # Encode
-# if (len(word) >= 3):
-#     a = (word[1:]+word[0])
-#     b = ''
-#     c = ''
-#     for i in range(3):
-#         b = b+random.choice(string.ascii_letters).lower()
-#         c = c+random.choice(string.ascii_letters).lower()
 
-#     print(c+a+b)
 
-# else:
-#     print(word[::-1])
+def encoding(w):
+    if (len(w) >= 3):
+        a = (w[1:]+word[0])
+        b = ''
+        c = ''
+        for i in range(3):
+            b = b+random.choice(string.ascii_letters).lower()
+            c = c+random.choice(string.ascii_letters).lower()
+
+        print(c+a+b)
+
+    else:
+        print(w[::-1])
 # decode
-if (len(word) < 3):
-    print(word[::-1])
+
+
+def decoding(w):
+    if (len(w) < 3):
+        print(w[::-1])
+    else:
+        result = w[3:-3]
+        a = result[4:]+result[:-1]
+        print(a)
+
+
+word = input("Enter a word:")
+option = int(input("Enter option 1.encoding 2.Decoding"))
+if (option == 1):
+    encoding(word)
+elif (option == 2):
+    decoding(word)
 else:
-    result = word[3:-3]
-    a = result[4:]+result[:-1]
-    print(a)
+    print("Enter 1 or 2")
